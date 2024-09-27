d2a80e21e6cb95919fab3f337ee51f93b3264336
Mansoor Hussain <qaisrani07@gmail.com>
2024-09-27 21:53:18 +0500

    add preview config

--- app/api/preview/route.js
+++ app/api/preview/route.js
@@ -1,9 +1,11 @@
 import { redirectToPreviewURL } from "@prismicio/next";
 
 import { createClient } from "../../../prismicio";
+import { draftMode } from "next/headers";
 
 export async function GET(request) {
   const client = createClient();
+  draftMode().enable();
 
   return await redirectToPreviewURL({ client, request });
 }
